9f081e721663431b61b54aa6121d4810622a8643
22jy0228 <22jy0228@PC04.jynet2.jec.ac.jp>
2023-10-23 14:10:28 +0900

    表示内容に苗字を追加

--- src/gitTag/tagTest.java
+++ src/gitTag/tagTest.java
@@ -4,7 +4,7 @@ public class tagTest {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("日本電子専門学校情報システム開発科");
+		System.out.println("日本電子専門学校情報システム開発科 富永");
 	}
 
 }
